d1f3fe5d2742332b6f88548c5bd497a0e7bcfd1d
Henri Bourcereau <henri.bourcereau@gmail.com>
2015-11-10 12:46:44 +0100

    fix arc circle

--- src/arbreintegralVizDriver.js
+++ src/arbreintegralVizDriver.js
@@ -52,11 +52,19 @@ export function makeVizDriver(AI){
   }
 
   function makeJoinLine(fromLeaf, toLeaf) {
-    const coordsFrom = AI.getCoords(fromLeaf);
-    const coordsTo = AI.getCoords(toLeaf);
+    let coordsFrom = AI.getCoords(fromLeaf);
+    let coordsTo = AI.getCoords(toLeaf);
 
     let joinLine;
     if (coordsFrom.circ == coordsTo.circ && coordsFrom.pos != coordsTo.pos){
+      //Reverse arc direction if destination is 'before' start leaf
+      let diff = coordsTo.pos - coordsFrom.pos;
+      if ( diff == -1 || diff > 1 ){
+        let tmp = coordsFrom;
+        coordsFrom = coordsTo;
+        coordsTo = tmp;
+      }
+
       joinLine = makeArcBetweenLeafs(coordsFrom, coordsTo);
       joinLine.stroke = color_brothers;
     } else {
@@ -73,14 +81,29 @@ export function makeVizDriver(AI){
     // const line = two.makeLine(posfrom.x, posfrom.y, posto.x, posto.y);
 
     const radius = Math.sqrt(Math.pow(posto.x - origin.x, 2) + Math.pow(posto.y - origin.y, 2));
-    let anglefrom = Math.acos((posfrom.x - origin.x)/radius);
-    let angleto = Math.acos((posto.y - origin.y)/radius);
+
+    let polarfrom = polarCoords({x: posfrom.x - origin.x, y:origin.y - posfrom.y});
+    let polarto = polarCoords({x: posto.x - origin.x, y:origin.y - posto.y});
+
     const line = two.makeArcSegment(
       origin.x, origin.y,
       radius, radius + 0.1,
-      anglefrom, angleto
+      // 0, twoAngle(polarto.angle - polarfrom.angle),
+      // 0, polarto.angle - polarfrom.angle,
+      0, polarfrom.angle - polarto.angle,
     );
+    line.rotation = -twoAngle(polarto.angle);
     return line;
+  }
+
+  function twoAngle(angle){
+    return Math.PI/2 + angle ;
+  }
+
+  function polarCoords(pos){
+    const r = Math.sqrt(Math.pow(pos.x, 2) + Math.pow(pos.y, 2)); 
+    const angle = 2 * Math.atan(pos.y/(pos.x + r));
+    return {r:r, angle:angle};
   }
 
   function makeLineBetweenLeafs (from, to){
