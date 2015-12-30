8e9a4795b2f59fa5e2b740a4a0830fb1159845d1
Henri Bourcereau <henri.bourcereau@gmail.com>
2015-12-30 12:32:26 +0100

    progression with divs

--- src/progressionComponent.js
+++ src/progressionComponent.js
@@ -5,7 +5,8 @@ export function progressionComponent(responses) {
   let props$ = responses.props.getAll();
   let vtree$ = props$.map(props => {
       // return h('progress', {max:props.max, value:props.value}, props.value);
-      return h('span', props.value.map(isUp => isUp?"-":"_").join(""));
+      // return h('span', props.value.map(isUp => isUp?"-":"_").join(""));
+      return h('div', {className: 'ai-progress'}, props.value.map(isUp => h("div", {className: isUp?"ai-progress-up":"ai-progress-down"})));
     });
 
   return {
