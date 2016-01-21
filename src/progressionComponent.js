969cd689b00f73cb7858a10f5bd9da83a363a4c0
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-01-21 10:56:31 +0100

    progression component style

--- src/progressionComponent.js
+++ src/progressionComponent.js
@@ -7,7 +7,9 @@ export function progressionComponent(responses) {
       // return h('progress', {max:props.max, value:props.value}, props.value);
       // return h('span', props.value.map(isUp => isUp?"-":"_").join(""));
       let progressElems = props.value.map(isUp => h("div", {className: isUp?"ai-progress-up":"ai-progress-down"}));
-      progressElems.push(h("div", {className: 'ai-progress-left'}));
+      for (let i = props.value.length; i < 127 ; i++){
+        progressElems.push(h("div", {className: 'ai-progress-remaining'}));
+      }
       return h('div', {className: 'ai-progress-container'}, [
           h('div', {className: 'ai-progress'}, progressElems)]);
     });
