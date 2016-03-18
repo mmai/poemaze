4153c64ab74780dfa70e7cc690d868686e202b9c
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-03-18 15:35:24 +0100

    worpress front-page template

---
+++ www/wp-content/themes/arbre-integral/front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+  <div id="app">
+
+    <div id="ai-page">
+      <div class="main-container">
+        <div class="home-content">
+          <img class="home-logo" src="/wp-content/themes/arbre-integral/img/assets/logo-home.svg"/>
+          <div class="home-title">
+            <div class="first-line">par</div>
+            <div class="second-line">Donatien Garnier</div>
+            <div class="third-line">graphisme Franck Tallon</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <script src="/wp-content/themes/arbre-integral/bundle.js"></script>
+</body>
+</html>
--- www/wp-content/themes/arbre-integral/functions.php
+++ www/wp-content/themes/arbre-integral/functions.php
@@ -1,4 +1,10 @@
 <?php 
+show_admin_bar(false);
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 function ai_breadcrumb(){
 	$showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
 	$delimiter   = ' / '; // delimiter between crumbs
